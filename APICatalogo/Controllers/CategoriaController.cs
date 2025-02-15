07854c491586fa1d12d9f7a74013bcc180a50e91
Gustavo Pilar <pilargustavo7@gmail.com>
2025-02-15 15:25:14 -0300

    Feat: Adicionar endpoints no CategoriaController

---
+++ APICatalogo/Controllers/CategoriaController.cs
@@ -0,0 +1,81 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if (categorias is null) return NotFound("Nenhuma categoria encontrada");
+
+            return categorias;
+        }
+
+        [HttpGet("{id:int}", Name="ObterCategoria")]
+        public ActionResult<Categoria> GetById(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault<Categoria>(c => c.Id == id);
+
+            if (categoria is null) return NotFound("Categoria não encontrada");
+
+            return categoria;
+        }
+
+        [HttpGet("Produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null) return BadRequest("Entidade inválida. Verifique e tente novamente.");
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.Id) return BadRequest("Id nao encontrado. Verifique e tente novamente...");
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.Id == id);
+
+            if (categoria is null) return BadRequest("Categoria não encontrada. Verifique e tente novamente");
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f355a08af08aa1bc368d77db2f1cd44a0a5b95e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71629a33b2f14f601a3b56491d7236cf39f4a125")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
