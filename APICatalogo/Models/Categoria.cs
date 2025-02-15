3bbcba060c5d505a242a1be68448311daeb1f70a
Gustavo Pilar <pilargustavo7@gmail.com>
2025-02-15 12:44:05 -0300

    Feat: Aplicar o Data Annotations

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
-
     public Categoria()
     {
         Produtos = new Collection<Produto>(); // É RESPONSABILIDADE DA CLASSE INICIAR A COLEÇÃO
     }
+
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? ImagemUrl { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public ICollection<Produto>? Produtos { get; set; }
 }
