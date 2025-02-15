6f355a08af08aa1bc368d77db2f1cd44a0a5b95e
Gustavo Pilar <pilargustavo7@gmail.com>
2025-02-15 14:27:13 -0300

    Feat: Criando o controlador ProdutosController

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        
+    }
+}
---
+++ APICatalogo/Migrations/20250215155236_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) VALUES ('Coca Cola Diet','Refrigereante Coca Cola Diet 350ml',5.45,'cocacoladiet.jpg',50,now(),1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) VALUES ('Lanche de Atum','Lanche de Atum com maoinese',8.50,'lancheatum.jpg',10,now(),2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) VALUES ('Pudim','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
